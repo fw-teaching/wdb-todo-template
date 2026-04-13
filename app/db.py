c610a6ff387e26df2715061595e8eefb962866bc
Fredrik Welander <76183002+welandfr@users.noreply.github.com>
2026-04-13 20:42:23 +0300

    Update database schema to include todo_users table
    Removed sample tables and added todo_users table.

--- app/db.py
+++ app/db.py
@@ -8,27 +8,13 @@ def get_conn():
 def create_schema():
     try:
         with get_conn() as conn, conn.cursor() as cur:
-            # Create the schema
             cur.execute("""
-                -- sample parent table
-                CREATE TABLE IF NOT EXISTS foo (
-                    id SERIAL PRIMARY KEY, -- primary key
-                    created_at TIMESTAMP DEFAULT now()
-                );
-                
-                -- sample child table
-                CREATE TABLE IF NOT EXISTS bar (
-                    id SERIAL PRIMARY KEY,
-                    foo_id INT REFERENCES foo(id), -- foreign key
-                    created_at TIMESTAMP DEFAULT now()
-                );
-
-                -- adding columns after the fact
-                ALTER TABLE foo ADD COLUMN IF NOT EXISTS name VARCHAR;
-                ALTER TABLE foo ADD COLUMN IF NOT EXISTS created_at TIMESTAMP DEFAULT now()""")
             
-            # insert sample data
-            cur.execute("""INSERT INTO foo (name) VALUES ('test')""")
+                CREATE TABLE IF NOT EXISTS todo_users (
+                    id SERIAL PRIMARY KEY,
+                    username VARCHAR NOT NULL
+                );
+                """)
 
     except Exception as e:
         print(f"Error while creating schema: {e}")
